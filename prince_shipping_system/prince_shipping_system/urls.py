679beb31e0c41f998f4a4cb15cefcc7a8f60e4f9
Rufaro Edward Mutepfa <45569487+Edwardrem@users.noreply.github.com>
2026-02-08 12:41:50 +0200

    mordenize the ui

--- prince_shipping_system/prince_shipping_system/urls.py
+++ prince_shipping_system/prince_shipping_system/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from shipping_system import views
 
@@ -57,3 +59,6 @@ urlpatterns = [
     path('my-account/edit/', views.edit_user_details, name='edit_user_details'),
     path('logout/', views.user_logout, name='logout'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
